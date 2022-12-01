b4127c90443c0058012c127f5ed2eecc44739517
Pavel <rut100500@yandex.ru>
2022-12-01 17:04:13 +0300

    Цикл и рекурсия вычислить Факториал

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вычислить Факториал
+
+// вариант через цикл
+Console.WriteLine(FactorialFor(10));
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+
+// вариант через рекурсию
+
+Console.WriteLine(FactorialRecursiv(11));
+
+int FactorialRecursiv(int n)
+{
+    if(n == 1) return 1;
+    else return n * FactorialRecursiv(n - 1);
+}

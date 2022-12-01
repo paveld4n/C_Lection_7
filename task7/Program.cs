615faeb2d5c52773e0fcfeb331abfc68958a2628
Pavel <rut100500@yandex.ru>
2022-12-01 17:09:35 +0300

    Игра в пирамидки. переместить блинчики с пирамиды 1 на кторой блины по ваозрастанию на пирамиду 3 с учетом возрастания

---
+++ task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Игра в пирамидки. переместить блинчики с пирамиды 1 на кторой блины по ваозрастанию на пирамиду 3 с учетом возрастания.
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+//          исходный шпиль    финальный шпиль  промежуточный шпиль  количество блинчиков
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers();

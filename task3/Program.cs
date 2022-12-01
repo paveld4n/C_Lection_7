afc4934c0687477992c2766354f0be66e1bcb4f1
Pavel <rut100500@yandex.ru>
2022-12-01 17:03:09 +0300

    Цикл и рекурсия нНайти сумму чисел от 1 до N

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму чисел от 1 до N
+
+// вариант 1 через цикл
+Console.WriteLine(SumFor(12));
+
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+
+// Вариант 2 через рекурсию
+Console.WriteLine(SumRecursiv(11));
+
+int SumRecursiv(int n)
+{
+    if (n == 0) return 0; // условние выхода из рекурсии (метода)
+    else return n + SumRecursiv(n - 1);
+}

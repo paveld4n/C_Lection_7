5767b8eac9c01787ebf93796b5613bbc68e00a5a
Pavel <rut100500@yandex.ru>
2022-12-01 16:59:51 +0300

    Цикл и рекурсия собрать строку с числами от a до b, a<=b

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Собрать строку с числами от a до b, a<=b
+
+// Вариант 1 Через цикл
+/*Console.Write(NumbersFor(1, 10));
+
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+*/
+// вариант 2 Через рекурсию
+
+Console.Write(NembersRecursiv(1, 15));
+
+string NembersRecursiv(int a, int b)
+{
+    if(a < b) return $"{a} " + NembersRecursiv(a + 1, b);
+    else return String.Empty; // необходимо указать условия выхода из метода
+}
